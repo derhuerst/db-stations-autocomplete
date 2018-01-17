45e7176ca91741c0ae846ee50ce00bedaf379e21
Jannis R <mail@jannisr.de>
2018-01-17 23:49:45 +0100

    db-stations@2, build: use synchronous-autocomplete

--- build.js
+++ build.js
@@ -2,7 +2,8 @@
 
 const fs = require('fs')
 const path = require('path')
-const stations = require('db-stations')
+const getStations = require('db-stations')
+const build = require('synchronous-autocomplete/build')
 const tokenize = require('tokenize-db-station-name')
 
 const showError = (err) => {
@@ -15,73 +16,36 @@ const writeJSON = (file, data, cb) => {
 	fs.writeFile(path.join(__dirname, file), JSON.stringify(data), cb)
 }
 
-const defaultWeight = 10 // todo: use the average weight instead
+console.info('Collecting search items.')
 
+// we map the IDs to get smaller file sizes
+const originalIds = Object.create(null)
+let currentId = 0
 
+const items = []
 
-const createIndex = (cb) => {
-	const index = Object.create(null)
+getStations()
+.on('data', (station) => {
+	const newId = (currentId++).toString(36)
+	originalIds[newId] = station.id
 
-	stations()
-	.on('data', (s) => {
-		const tokens = tokenize(s.name)
-		index[s.id] = {
-			n: s.name,
-			w: s.weight || defaultWeight,
-			t: tokens.length
-		}
+	items.push({
+		id: newId,
+		name: station.name,
+		weight: station.weight
 	})
-	.once('end', () => cb(null, index))
-	.once('error', cb)
-}
-
-const createTokensList = (cb) => {
-	const allTokens = Object.create(null)
-
-	stations()
-	.on('data', (s) => {
-		const tokens = tokenize(s.name)
-		for (let t of tokens) {
-			if (!(t in allTokens)) allTokens[t] = []
-			allTokens[t].push(s.id)
-		}
-	})
-	.once('end', () => cb(null, allTokens))
-	.once('error', cb)
-}
-
-const computeTokenScores = (tokensList) => {
-	const scores = Object.create(null)
-	const nrOfAllStations = Object.keys(tokensList).length
-
-	for (let t in tokensList) {
-		const nrOfStations = tokensList[t].length
-		scores[t] = nrOfStations / nrOfAllStations
-	}
-
-	let max = 0
-	for (let t in scores) max = Math.max(scores[t], max)
-
-	for (let t in scores) {
-		let score = (max - scores[t]) / max // revert, clamp to [0, 1]
-		score = Math.pow(score, 5) // stretch distribution
-		scores[t] = parseFloat(score.toFixed(5))
-	}
-
-	return scores
-}
-
-
-
-createIndex((err, index) => {
-	if (err) return showError(err)
-
-	writeJSON('stations.json', index, showError)
 })
+.once('end', () => {
+	console.info('Computing a search index.')
 
-createTokensList((err, tokensList) => {
-	if (err) return showError(err)
+	const {tokens, scores, weights, nrOfTokens} = build(tokenize, items)
 
-	writeJSON('tokens.json', tokensList, showError)
-	writeJSON('scores.json', computeTokenScores(tokensList), showError)
+	console.info('Writing the index to disk.')
+
+	writeJSON('original-ids.json', originalIds, showError)
+	writeJSON('tokens.json', tokens, showError)
+	writeJSON('scores.json', scores, showError)
+	writeJSON('weights.json', weights, showError)
+	writeJSON('nr-of-tokens.json', nrOfTokens, showError)
 })
+.once('error', showError)
