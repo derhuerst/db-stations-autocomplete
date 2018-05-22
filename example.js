07f8317bea68d0636995526c5d7ad2322ee62a34
Jannis R <mail@jannisr.de>
2018-05-22 16:25:11 +0200

    interactive demo

--- example.js
+++ example.js
@@ -1,6 +1,49 @@
 'use strict'
 
+const readStations = require('db-stations')
+const prompt = require('cli-autocomplete')
+
 const autocomplete = require('.')
 
-console.log(autocomplete('Münch', 3))
-// console.log(autocomplete('Frankfrut Hbf', 3, true)) // typo
+const pStations = new Promise((resolve, reject) => {
+	const res = Object.create(null)
+	readStations()
+	.on('data', (s) => {
+		res[s.id] = s
+	})
+	.once('end', () => {
+		resolve(res)
+	})
+	.once('error', reject)
+})
+
+const suggest = (input) => {
+	return pStations
+	.then((stationsById) => {
+		const results = autocomplete(input, 5)
+		const choices = []
+
+		for (let result of results) {
+			const station = stationsById[result.id]
+			if (!station) continue
+
+			choices.push({
+				title: [
+					station.name,
+					'–',
+					'score:', result.score.toFixed(3),
+					'relevance:', result.relevance.toFixed(3)
+				].join(' '),
+				value: station.id
+			})
+		}
+
+		return choices
+	})
+}
+
+prompt('Type a station name!', suggest)
+.once('abort', () => {
+	process.exitCode = 1
+})
+.once('submit', id => console.log(id))
